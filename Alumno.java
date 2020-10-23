ada4d086b52150430198cc7fc710cfc1bdba2764
diegodog2312 <54220428+diegodog2312@users.noreply.github.com>
2020-10-23 15:18:08 -0500

    Java

---
+++ Alumno.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1eda2;
+
+/**
+ *
+ * @author diegoignacionunezhernandez
+ */
+public class Alumno {
+    private String nombre;
+    private String apellido;
+    private int noCuenta;
+
+    public Alumno(String nombre, String apellido, int noCuenta) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.noCuenta = noCuenta;
+    }    
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getNoCuenta() {
+        return noCuenta;
+    }
+
+    public void setNoCuenta(int noCuenta) {
+        this.noCuenta = noCuenta;
+    }        
+}
---
+++ Dato.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1eda2;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * En esta clase se obtienen los datos de un archivon de texto
+ * @author diegoignacionunezhernandez
+ */
+public class Dato {
+    public ArrayList leerDato(int inicio, int fin, String archivoE) throws FileNotFoundException, IOException{
+        ArrayList<Alumno> datos = new ArrayList<>();
+        String cadena;
+        String cadenas[];
+        
+        File archivo = new File(archivoE);        
+        FileReader lector = new FileReader(archivo); 
+        BufferedReader lectura = new BufferedReader(lector);
+                
+        for(int i=0; i<inicio; i++){
+            lectura.readLine();
+        }
+        while(inicio<fin){            
+            cadena = lectura.readLine();
+            cadenas = cadena.split(",");        
+            Alumno alumno = new Alumno(cadenas[0], cadenas[1], Integer.valueOf(cadenas[2]));
+            datos.add(alumno);
+            inicio++;
+        }
+               
+        lector.close();
+        lectura.close();
+        return datos;
+    }
+    
+    public void escribirDatos(ArrayList<Alumno> alumnos, String archivoD) throws IOException{
+        File archivo = new File(archivoD);     
+        FileWriter escribir = new FileWriter(archivo);
+        for(int i=0; i<alumnos.size(); i++){
+            escribir.write(alumnos.get(i).getNombre().concat(",")
+                    .concat(alumnos.get(i).getApellido().concat(",")
+                        .concat(String.valueOf(alumnos.get(i).getNoCuenta()))));          
+        }
+         escribir.close();
+    }
+}
